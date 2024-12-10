2eaedf5737415fdc48e1384c952bdb7f39c49bb9
Rolf Ivo Marquardt Junior <rolf.junior@TECNOWISE.LAN>
2024-12-10 18:21:41 -0300

    ajustes para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitro/plugin-react'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
+  server: {
+    port: 3000,
+    open: true
+  }
 }) 

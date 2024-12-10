aa7bb691e5cfe2bd41774fc070d7062425b0cf74
Rolf Ivo Marquardt Junior <rolf.junior@TECNOWISE.LAN>
2024-12-10 19:13:39 -0300

    deploy2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vida-cidada/',
+  base: '/vida/',
   server: {
     port: 3000,
     open: true
